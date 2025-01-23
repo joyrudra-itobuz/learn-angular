a6957d9e372e259de12b5ed40033566dfebb0c45
Joyrudra Biswas <joyrudra@snapecabs.com>
2025-01-23 14:40:20 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [UserComponent],
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  message = 'Mouse Over to see the message';
+  operatingSystems = [
+    { id: 'win', name: 'Windows' },
+    { id: 'osx', name: 'MacOS' },
+    { id: 'linux', name: 'Linux' },
+  ];
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
+  isEditable = true;
+  imageURL =
+    'https://images.pexels.com/photos/24029951/pexels-photo-24029951/free-photo-of-case-and-earbuds.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+  onMouseOver() {
+    this.message = 'Way to go 🚀';
+  }
+  onMouseLeave() {
+    this.message = 'Mouse Over to see the message';
+  }
 }
